357aa45781230191045b48d25fc14ba869125913
thederpykrafter <thederpykrafter@gmail.com>
2024-08-26 00:36:36 -0400

    draw triangle

--- graphics.lua
+++ graphics.lua
@@ -3,16 +3,20 @@ local M = {}
 M.size = {}
 
 function M.setDefaults()
-  love.graphics.setColor(0,0,0)
-  love.graphics.setBackgroundColor(1,1,1)
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.setBackgroundColor(1, 1, 1)
 end
 
 function M.rgbColor(r, g, b, a)
-  love.graphics.setColor(r/255, g/255, b/255, a/255)
+	love.graphics.setColor(r / 255, g / 255, b / 255, a / 255)
 end
 
 function M.rgbBgColor(r, g, b, a)
-  love.graphics.setBackgroundColor(r/255, g/255, b/255, a/255)
+	love.graphics.setBackgroundColor(r / 255, g / 255, b / 255, a / 255)
+end
+
+function M.drawTriangle(p1, r)
+	love.graphics.circle("line", p1[1], p1[2], r, 3)
 end
 
 return M
--- main.lua
+++ main.lua
@@ -5,17 +5,20 @@ local size = graphics.size
 
 ---- Love Functions ----
 function love.load()
-  graphics.setDefaults()
+	graphics.setDefaults()
 end
 
 function love.update(dt)
-size.width , size.height = love.graphics.getDimensions()
+	size.width, size.height = love.graphics.getDimensions()
 end
 
 function love.draw()
-  graphics.setDefaults()
-  love.graphics.print("Learning Love", 10, 10)
-  love.graphics.setColor(1, 1, 1)
-  love.graphics.rectangle("line", 0, 0, size.width, size.height)
+	graphics.setDefaults()
+	love.graphics.print("Learning Love", 10, 10)
+	love.graphics.setColor(1, 0.5, 0)
+	love.graphics.setLineWidth(4)
+	love.graphics.rectangle("line", 0, 0, size.width, size.height)
+	love.graphics.setLineWidth(2)
+	graphics.drawTriangle({ size.width / 2, size.height / 2 }, 50)
 end
 ---- End Love Functions ----
